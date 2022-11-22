99aa46c0cf1758f9fb414f466db17b93f3304152
aeoh <aline.sanches@gft.com>
2022-11-22 14:19:10 -0300

    creates members endpoint

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+  name = models.CharField(max_length=100)
+  lastName = models.CharField(max_length=100)
+  phone = models.CharField(max_length=11)
+  email = models.EmailField()
+  address = models.CharField(max_length=100)
+  photo = models.ImageField(upload_to='members_profile')
+  
+  def __str__(self) -> str:
+    return self.name + "  " + self.lastName
+  
+  
